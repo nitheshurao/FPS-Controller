737d422deb544b16adc56abe495459e94a1a5805
nitheshurao <31339538+nitheshurao@users.noreply.github.com>
2021-04-15 23:43:31 +0530

    Unity 3D FPS Controller

---
+++ Assets/moselok.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moselok : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float mousesencitivity = 100f;
+    public Transform playerbody;
+    float xrotaion = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousex = Input.GetAxis("Mouse X")* mousesencitivity *Time.deltaTime;
+        float mousey = Input.GetAxis("Mouse Y")* mousesencitivity* Time.deltaTime;
+
+
+        xrotaion -= mousey;
+        xrotaion = Mathf.Clamp(xrotaion, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xrotaion, 0f, 0f);
+        playerbody.Rotate(Vector3.up * mousex);
+    }
+
+}
---
+++ Assets/playermovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    Vector3 velocity;
+
+
+    public Transform groundcheck;
+    public float grounddistance = 0.04f;
+    public LayerMask groundMask;
+    public float jumpheight = 3f;
+
+    bool isGrounded;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundcheck.position, grounddistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+
+        //if(Input.GetButtonDown("jump")&& isGrounded)
+        //{
+        //    velocity.y = Mathf.Sqrt(jumpheight * -2 * gravity);
+        //}
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move *speed *Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
